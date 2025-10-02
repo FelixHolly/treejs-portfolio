a824359eff745997d6dc76876b8761e08e6ec511
Felix <felixhollndonner@hotmail.com>
2025-10-02 08:40:02 +0200

    added env files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  emailJs: {
+    serviceId: "service_cvyf4pk",
+    templateId: "template_1cbxfpl",
+    publicKey: "uxjMVOPJ12-pBwK7L",
+  },
+  assets: {
+    dracoPath: "assets/draco/",
+    models: {
+      heroModel: "assets/models/vienna-lowe.glb",
+      projectPhone: "assets/models/future-phone.glb",
+    },
+  },
+};
